ad93e72df871ff4c5e26357e2ae351934db557b6
KeychainOcelot <mysticshunal@gmail.com>
2022-12-22 18:54:32 +0530

    initial commit

---
+++ Mob.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+func _ready():
+	$AnimatedSprite.playing = true
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+#func _process(delta):
+#	pass
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
